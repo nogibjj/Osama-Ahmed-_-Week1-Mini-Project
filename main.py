f7d8bcfb040dbf95820b0c895c9bb5e788250777
osama-shawir <89782802+osama-shawir@users.noreply.github.com>
2023-09-03 23:24:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-def factorial(x):
+def IntFactorial(x):
     """This is a recursive function
     to find the factorial of an integer"""
     if x == 1:
         return 1
     else:
         # recursive call to the function
-        return (x * factorial(x-1))
+        return (x * IntFactorial(x-1))
